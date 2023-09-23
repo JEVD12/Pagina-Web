3ab18807ebadde6ddcd751e49a46727e72f1a8fd
UCLAB102 <n00037678@upn.pe>
2023-09-23 16:48:50 -0500

    VERSION2

---
+++ JS/Funciones.js
@@ -0,0 +1,7 @@
+function Saludar(){
+ var  nombre=document.getElementById('txtNombre').value;
+ alert("HOLA  " + nombre +", BIENVENIDO !!");
+}
+function Resetear (){
+    document.getElementById('txtNombre').value="";
+}
